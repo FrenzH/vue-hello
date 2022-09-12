d70de3e7af87b1cce6e70ef8265e95bbbcc825d6
FrenzH <franciattom99@gmail.com>
2022-09-12 19:20:16 +0100

    get-data

---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el:"#root",
+        data:{
+            h1:"Hello world!",
+            img:'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
+        }
+    }
+)
