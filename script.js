007abd4f99092bd8f0348b003a59610b8dee6fe3
FrenzH <franciattom99@gmail.com>
2022-09-13 12:32:33 +0100

    vuejs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const app = new Vue (
     {
-        el:"#root",
+        el:"#app",
         data:{
             h1:"Hello world!",
-            img:'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
+            img:'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+            color:['red','blue']
         }
     }
 )
